de8b851ea4422f4fa3a23b57fc85d391c985597e
Richard Kimmel <rkimmel@peachtreebilling.com>
2015-09-17 11:13:06 -0400

    add layout script

---
+++ layoutstartup.sh
@@ -0,0 +1,112 @@
+#! /bin/bash
+
+xdotool key Super+Return
+
+sleep .30
+
+xdotool type 'screenfetch'
+
+sleep .30
+
+xdotool key Return
+
+sleep .10
+
+xdotool key Super+h
+
+sleep .10
+
+xdotool key Super+Return
+
+sleep .10
+
+xdotool key Super+r
+
+sleep .10
+
+xdotool key Left
+
+sleep .10
+
+xdotool key Escape
+
+sleep .10
+
+xdotool type 'profanity'
+
+sleep .10
+
+xdotool key Return
+
+sleep .10
+
+xdotool key Super+v
+
+sleep .10
+
+xdotool key Super+Return
+
+sleep .10
+
+xdotool key Super+r
+
+sleep .10
+
+xdotool key Up
+
+sleep .10
+
+xdotool key Escape
+
+sleep .10
+
+xdotool type 'ranger'
+
+sleep .10
+
+xdotool key Return
+
+sleep .10
+
+xdotool key Super+h
+
+sleep .10
+
+xdotool key Super+Return
+
+sleep .10
+
+xdotool type 'clear && td'
+
+sleep .10
+
+xdotool key Return
+
+sleep 5
+
+xdotool key Super+4
+
+sleep .10
+
+xdotool key Super+Return
+
+sleep .30
+
+xdotool type 'weechat'
+
+sleep .30
+
+xdotool key Return
+
+sleep .10
+
+xdotool key Super+1
+
+sleep .10
+
+xdotool key Super+Left
+
+sleep .10 
+
+xdotool key Super+Left
+
